2aca495557c67fc50a6a26963950957d259b0cfc
Shresth Rajput <rajshresth2@gmail.com>
2026-04-22 15:06:06 +0530

    Spread Props

--- src/Topic-2 Props/Parent.jsx
+++ src/Topic-2 Props/Parent.jsx
@@ -2,26 +2,34 @@ import Card from "./Card";
 
 const Parent = () => {
 
-    const player = {
+    const player1 = {
         username: "Rohit Sharma",
         age: 38,
         team: "MI",
         jersy: 45,
-        skill: ["batting","bowling", "captaincy"],
-        address: {
-            country: "India",
-            state: {
-                name: "Maharashtra",
-                city: "Mumbai"
-            },
-        }
+        skill: "batting",        
+    }    
+
+    const player2 = {
+        username: "Virat Kohli",
+        age: 36,
+        team: "RCB",
+        jersy: 18,
+        skill: "batting",        
     }
 
+    const player3 = {
+        username: "Mahendra Singh Dhoni",
+        age: 43,
+        team: "CSK",
+        jersy: 7,
+        skill: "batting",        
+    }
+        
     return <div className="parent-component">
-            
-            {/* <Card username="Virat Kohli" age={36} team="RCB" skill="batting" jersy={18} /> */}
-
-            <Card player={player} />
+            <Card {...player1} />
+            <Card {...player2} />
+            <Card {...player3} />
     </div>
 }
 

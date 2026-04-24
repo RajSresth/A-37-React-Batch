7620724b520fabd83dfccdd1383d493fa5e173c6
Shresth Rajput <rajshresth2@gmail.com>
2026-04-24 15:07:41 +0530

    Render Props and map fn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Parent from "./Topic-4 map fn/Parent"
+import UL from "./Topic-5 map fn revision/UL"
 
 const App = () => {
   return (
-    <div className="app-component">
-        <Parent/>
+    <div id="app-component">
+        <UL/>
     </div>
   );
 };

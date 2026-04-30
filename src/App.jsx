506583c14777aa89d5acf5a2fc14d8c1a94677ee
Shresth Rajput <rajshresth2@gmail.com>
2026-04-30 14:58:59 +0530

    useState Hook Part-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// import Counter from "./Topic-7 React state variable/Counter"
-import Home from "./Topic-8 Conditional Rendering/Home";
+
+// import Toggle from "./Topic-9 conditional Rendering-2/Toggle"
+import Counter from "./Topic-10 Lazy Initialization/Counter"
 
 const App = () => {
   
   return (
     <div id="app">
-        {/* <Counter /> */}
-        <Home/>
+        {/* <Toggle/> */}
+      <Counter/>
     </div>
   );
 };

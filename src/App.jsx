b8eee36d6e3d2e965149487ddc66afd8fc592d0e
Shresth Rajput <rajshresth2@gmail.com>
2026-05-04 15:00:46 +0530

    useEffect Real world

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
-// import Toggle from "./Topic-9 conditional Rendering-2/Toggle"
-import Counter from "./Topic-10 Lazy Initialization/Counter"
+// import Toggle from "./Topic-11 useEffect Hook/Toggle"
+// import Counter from "./Topic-10 Lazy Initialization/Counter"
+import Shop from "./Topic-12 useEffect Realword/Shop";
+
 
 const App = () => {
   
   return (
     <div id="app">
-        {/* <Toggle/> */}
-      <Counter/>
+        <Shop/>     
     </div>
   );
 };

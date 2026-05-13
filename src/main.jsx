a019d4e785cc92cb72a4e893831a1b523ad2e4b0
Shresth Rajput <rajshresth2@gmail.com>
2026-05-13 15:05:56 +0530

    Protected Route

--- src/main.jsx
+++ src/main.jsx
@@ -6,42 +6,58 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./Topic-14 React Routing/pages/Home";
 import Profile from "./Topic-14 React Routing/pages/Profile";
-import Shop from "./Topic-14 React Routing/pages/Shop"
-import Order from "./Topic-14 React Routing/pages/Order"
+import Shop from "./Topic-14 React Routing/pages/Shop";
+import Order from "./Topic-14 React Routing/pages/Order";
 import NotFound from "./Topic-14 React Routing/pages/NotFound";
 import ProductPage from "./Topic-14 React Routing/pages/ProductPage";
+
+// auth pages
+import Login from "./Topic-14 React Routing/pages/Login";
+
+// Routes
+import ProtectedRoute from "./Topic-14 React Routing/routes/ProtectedRoute";
+
 // Routing configuration
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     children: [
       {
         index: true,
         element: <Home />,
       },
       {
-        path: "profile",
-        element: <Profile />,
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "shop",
+            element: <Shop />,
+          },
+          {
+            path: "order",
+            element: <Order />,
+          },
+        ],
       },
       {
-        path: "shop",
-        element: <Shop />,
+        path: "products/:productId",
+        element: <ProductPage />,
       },
-      {
-        path: "order",
-        element: <Order />,
-      },
-      {
-        path: "products/:id",
-        element:<ProductPage/>
-      }
     ],
   },
   {
+    path: "/login",
+    element: <Login />,
+  },
+  {
     path: "*",
-    element: <NotFound/>
-  }
+    element: <NotFound />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(

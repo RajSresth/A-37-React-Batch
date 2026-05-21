ee9c7e6390d5fd39907a517f4e8624b962585c16
Shresth Rajput <rajshresth2@gmail.com>
2026-05-21 14:41:53 +0530

    Advanced useMemo

--- src/Topic-14 React Routing/components/Child.jsx
+++ src/Topic-14 React Routing/components/Child.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
-const Child = () => {
+const Child = ({result}) => {
   console.log("Child Render");
   return (
     <div>
       <h1 style={{ textAlign: "center", marginTop: "40px" }}>
         Child Component
       </h1>
+      <h3>Heavy Calculation Result: {result}</h3>
     </div>
   );
 };
 
 export default React.memo(Child);
+
+
+/**
+export default React.memo(Child, 
+  (prev,next) => prev.user.fullname === next.user.fullname);
+ */  
+// prevProps vs nextProp
+/**
+ * prevProps = "Shresth Rajput"
+ * nextProps = "Shresth Rajput"
+ * 
+ * 
+ * prevProps =  [01x1]-{fullname: "Sid Rajput"}
+ * nextProps =  [02x2]-{fullname: "Sid Rajput"}
+ * 
+ * ?  - optional chaining
+ * ?? - Nullish Coalceing operator
+ */
--- src/Topic-14 React Routing/pages/Order.jsx
+++ src/Topic-14 React Routing/pages/Order.jsx
@@ -1,36 +1,50 @@
-import {useState,useRef} from "react";
-import { useOutletContext } from "react-router-dom";
+import {useState, useMemo} from "react";
 import Child from "../components/Child";
 
 const Order = () => {
-  const [count,setCount] = useState(0);
-  const count2 = useRef(0);  
+  const [login, setLogin] = useState(false);
+  const [input, setInput] = useState(""); // input = 123456
+  console.log("Parent Render");
 
-  console.log("Order Render");
+  const heavyCalculation = (n) => {
+    console.log("Heavy Calculation")
+    if (!n || n <= 0) return null;
 
-  const handleClick = () => {
-    count2.current = count2.current + 1;
-  }
+    let count = 0;
+    let num = 1;
 
+    while (count < n) {
+      num++;
+      let isPrime = true;
+
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+
+      if (isPrime) count++;
+    }
+
+    return num;
+  };
+
+  const result =  useMemo(() => heavyCalculation(+input),[input]);
 
   return (
     <div id="order">
       <div className="center-box">
-        <h1>Welcome to Order Page</h1>
-
-        {/* state variable */}
-        <div className="state-count">
-          <h2>Count: {count}</h2>
-          <button onClick={() => setCount(count + 1)}>Add Count</button>
-        </div>
-
-        {/* Ref variable */}
-        <div className="ref-count">
-          <h2>Count2: {count2.current}</h2>
-          <button onClick={handleClick}>Add Count</button>
-        </div>
-
-        <Child/>
+        {login ? <h1>Welcome to Order Page</h1> : <h1>Please Login</h1>}
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter n to find nth prime"
+          style={{padding: "12px 16px", fontSize:"16px"}}
+        />
+        <button onClick={() => setLogin(!login)}>Click</button>
+        <Child result={result} />
       </div>
     </div>
   );
